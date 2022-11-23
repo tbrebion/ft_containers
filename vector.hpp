8787a85f66e60000365607a88a85e33215c86dea
Titouan BREBION <tbrebion@e2r4p17.clusters.42paris.fr>
2022-11-23 17:01:30 +0100

    wip

--- vector.hpp
+++ vector.hpp
@@ -6,7 +6,7 @@
 /*   By: tbrebion <tbrebion@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/18 11:54:23 by tbrebion          #+#    #+#             */
-/*   Updated: 2022/11/23 12:11:19 by tbrebion         ###   ########.fr       */
+/*   Updated: 2022/11/23 16:53:46 by tbrebion         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,24 +21,85 @@
 #include <memory>
 #include <stdexcept>
 #include <utility>
-
+#include "utils/enable_if.hpp"
+#include "utils/equal.hpp"
+#include "utils/is_integral.hpp"
+#include "utils/iterator_traits.hpp"
+#include "utils/lexicographical_compare.hpp"
+#include "utils/normal_iterator.hpp"
+#include "utils/pair.hpp"
+#include "utils/reverse_iterator.hpp"
 
 namespace ft{
 	
-	template<typename T, typename Allocator = std::allocator<T>>
+	template<typename Tp, typename Allocator = std::allocator<Tp>>
 	class vector{
 
 		public:
-			typedef T value_type;
-			typedef Allocator allocator_type;
-			typedef typename allocator_type::size_type size_type;
-			typedef typename allocator_type::difference_type difference_type; 
-			typedef typename allocator_type::reference reference;
-			typedef typename allocator_type::const_reference const_reference;
-			typedef typename allocator_type::pointer pointer;
-			typedef typename allocator_type::const_pointer const_pointer;
+		
+			typedef typename Allocator::value_type 			value_type;
+			typedef typename Allocator::pointer 			pointer;
+			typedef typename Allocator::const_pointer 		const_pointer;
+			typedef typename Allocator::reference 			reference;
+			typedef typename Allocator::const_reference 	const_reference;
+			typedef typename Allocator::size_type 			size_type;
+			typedef typename Allocator::difference_type 	difference_type;
+
+			typedef ft::normal_iterator<pointer, vector> 		iterator;
+			typedef ft::normal_iterator<const_pointer, vector> 	const_iterator;
+			typedef ft::reverse_iterator<iterator>		 		reverse_iterator;
+			typedef ft::reverse_iterator<const_iterator>		const_reverse_iterator;
+
+			typedef Allocator		allocator_type;
+			Allocator				vector_allocator;
 			
-	};	
+		private:
+		
+			pointer	V_start;
+			pointer	V_finish;
+			pointer	V_end_of_storage;
+
+		protected:	
+		
+			void	_Range_check(size_type n)const{
+				
+				if (n >= this->size()){
+
+					throw std::out_of_range("Out of Range error : vector::_Range_check");
+				}
+			}
+		
+		public:
+		
+			vector() : V_start(NULL), V_finish(NULL), V_end_of_storage(NULL){
+				
+			}
+
+			explicit vector(size_t n, const Tp &value = Tp()){
+				
+				this->V_start = vector_allocator.allocate(n);
+				this->V_finish = this->V_start + n;
+				this->V_end_of_storage = this->V_finish;	
+				for (pointer x = this->V_start; n != 0; --n, ++x){
+
+					vector_allocator.construct(x, value);
+				}
+			}
+
+			vector(const vector &x){
+
+				this->V_start = vector_allocator.allocate(x.end() - x.begin());
+				pointer y = this->V_start;
+				for (pointer z = x.V_start; z != x.V_finish; ++z, ++y){
+
+					vector_allocator.construct(y, *z);
+				}
+				this->V_finish = y;
+				this->V_end_of_storage = this->V_finish;
+			}
+			
+			
+	};
 }
 
 #endif
