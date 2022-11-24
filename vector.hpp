fbcfca58248469250db51fd0d487e62bfb427282
Titouan BREBION <tbrebion@e3r7p21.clusters.42paris.fr>
2022-11-24 15:47:10 +0100

    wip

--- vector.hpp
+++ vector.hpp
@@ -6,7 +6,7 @@
 /*   By: tbrebion <tbrebion@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/18 11:54:23 by tbrebion          #+#    #+#             */
-/*   Updated: 2022/11/23 16:53:46 by tbrebion         ###   ########.fr       */
+/*   Updated: 2022/11/24 15:46:43 by tbrebion         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -86,7 +86,7 @@ namespace ft{
 				}
 			}
 
-			vector(const vector &x){
+			vector(const vector<Tp> &x){
 
 				this->V_start = vector_allocator.allocate(x.end() - x.begin());
 				pointer y = this->V_start;
@@ -98,7 +98,198 @@ namespace ft{
 				this->V_end_of_storage = this->V_finish;
 			}
 			
+			template<InputIterator>
+			vector(InputIterator first, 
+				typename ft::enable_if<!ft::is_integral<InputIterator>::value, InpuIterator>::type last,
+					const Allocator& = Allocator()){
+
+				this->V_start = vector_allocator.allocate(ft::distance(first, last));
+				pointer x = this->V_start;
+			}
 			
+			vector	&operator=(const vector &x){
+
+				if (this == &x){
+					
+					return (*this);
+				}
+				clear();
+				vector_allocator.deallocate(this->V_start, this->V_end_of_storage - this->V_start);
+				pointer tmp = vector_allocator.allocate(x.size());
+				pointer y = tmp;
+				for (const_iterator it = x.begin(); it != x.end(); ++it, ++y){
+
+					vector_allocator.construct(y, *it);
+				}
+				this->V_start = tmp;
+				this->V_finish = tmp + x.size();
+				this->V_end_of_storage = tmp + x.capacity();
+				return (*this);
+			}
+
+			~vector(){
+
+				clear();
+				vector_allocator.deallocate(this->V_start, this->V_end_of_storage - this->V_start);
+			}
+			
+			iterator	begin(){
+
+				return (iterator(this->V_start));
+			}
+			
+			const_iterator	begin()const{
+
+				return (const_iterator(this->V_start));
+			}
+
+			iterator	end(){
+
+				return (iterator(this->V_finish));
+			}
+
+			const_iterator	end()const{
+
+				return (const_iterator(this->V_finish));
+			}
+
+			reverse_iterator	rbegin(){
+
+				return (reverse_iterator(end()));
+			}
+
+			const_reverse_iterator	rbegin()const{
+
+				return (const_reverse_iterator(end()));
+			}
+			
+			reverse_iterator	rend(){
+
+				return (reverse_iterator(begin()));
+			}
+			
+			const_reverse_iterator	rend()const{
+
+				return (const_reverse_iterator(begin()));
+			}
+
+			size_type	size()const{
+
+				return (size_type(this->V_finish - this->V_start));
+			}
+
+			size_type	max_size()const{
+
+				return (vector_allocator.max_size());
+			}
+
+			size_type	capacity()const{
+
+				return (this->V_end_of_storage - V_start);
+			}
+
+			bool	empty()const{
+
+				return (this->V_start == this->V_finish);
+			}
+
+			void	reserve(size_type n){
+
+				if (n > max_size()){
+
+					return;
+				}
+				if (n > capacity()){
+
+					pointer x = vector_allocator.allocate(n);
+					pointer y = x;
+					for (pointer ptr = this->V_start; ptr != this->V_finish; ++ptr, ++y){
+
+						vector_allocator.construct(y, *ptr);
+					}
+					clear();
+					vector_allocator.deallocate(this->V_start, this->V_end_of_storage - this->V_start);
+					this->V_start = x;
+					this->V_finish = y;
+					this->V_end_of_storage = this->V_start + n;
+				}
+			}
+
+			void	resize(size_type n, value_type val = value_type()){
+
+				if (n < size()){
+
+					for (pointer i = this->V_start + n; i != this->V_finish; ++i){
+						
+						vector_allocator.destroy(i);
+					}
+					this->V_finish = this->V_start + n;
+				}
+				else if (n > size()){
+
+					insert(end(), n - size(), val);
+				}
+			}
+
+			reference	operator[](size_type n){
+
+				return (*(this->V_start + n));
+			}
+						
+			const_reference	operator[](size_type n)const{
+
+				return (*(this->V_start + n));
+			}
+
+			reference	at(size_type n){
+
+				_Range_check(n);
+				return ((*this)[n]);
+			}
+			
+			const_reference		at(size_type n)const{
+
+				_Range_check(n);
+				return ((*this)[n]);
+			}
+			
+			reference	front(){
+
+				return (*begin());
+			}
+			
+			const_reference	front()const{
+
+				return (*begin());
+			}
+			
+			reference	back(){
+
+				return (*(end() - 1))
+			}
+			
+			const_reference		back()const{
+
+				return (*(end() - 1))
+			}
+
+			iterator	erase(iterator position){
+
+				if (position + 1 != end()){
+
+					for (pointer x = position.base() + 1, y = position.base(); x != this->V_finish; ++x, ++y){
+
+						*y = x;
+					}
+				}
+				vector_allocator.destroy(--this->V_finish);
+				return (position);
+			}
+
+			iterator	erase(iterator first, iterator last){
+
+				
+			}
 	};
 }
 
