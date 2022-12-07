d1e721610faa9f0a86c711d682a7a44a071f1376
Titouan BREBION <tbrebion@e3r2p15.clusters.42paris.fr>
2022-12-07 18:07:01 +0100

    wip

--- map.hpp
+++ map.hpp
@@ -6,29 +6,22 @@
 /*   By: tbrebion <tbrebion@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/30 14:30:23 by tbrebion          #+#    #+#             */
-/*   Updated: 2022/11/30 14:31:08 by tbrebion         ###   ########.fr       */
+/*   Updated: 2022/12/07 18:04:49 by tbrebion         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MAP_HPP
 #define MAP_HPP
 
-#include <iostream>
-#include <algorithm>
-#include <cstddef>
-#include <iterator>
-#include <limits>
-#include <memory>
-#include <stdexcept>
-#include <utility>
-#include "utils/enable_if.hpp"
-#include "utils/equal.hpp"
-#include "utils/is_integral.hpp"
-#include "utils/iterator_traits.hpp"
-#include "utils/lexicographical_compare.hpp"
-#include "utils/normal_iterator.hpp"
-#include "utils/pair.hpp"
-#include "utils/reverse_iterator.hpp"
-#include "utils/utils.hpp"
+#include "vector.hpp"
+
+namespace ft{
+
+	class map{
+
+		
+	};
+	
+}
 
 #endif
--- stack.hpp
+++ stack.hpp
@@ -6,14 +6,13 @@
 /*   By: tbrebion <tbrebion@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/18 12:20:52 by tbrebion          #+#    #+#             */
-/*   Updated: 2022/12/06 17:45:48 by tbrebion         ###   ########.fr       */
+/*   Updated: 2022/12/07 17:48:22 by tbrebion         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef STACK_HPP
 #define STACK_HPP
 
-#include <iostream>
 #include "vector.hpp"
 
 namespace ft{
@@ -21,8 +20,103 @@ namespace ft{
 	template<typename Tp, typename Container = vector<Tp> >
 	class	stack{
 
+		public:
+			typedef Container							container_type;
+			typedef typename container_type::value_type	value_type;
+			typedef typename container_type::size_type	size_type;
+			
+		protected:
+			container_type	c;
 		
+		public:
+			explicit stack(const Container &c = Container()) : c(c) {
+
+			}
+
+			bool	empty()const{
+
+				return (c.empty());
+			}
+
+			size_type	size()const{
+
+				return (c.size());
+			}
+
+			value_type	&top(){
+
+				return (c.back());
+			}
+
+			const value_type	&top()const{
+
+				return (c.back());			
+			}
+
+			void	push(const value_type &v){
+
+				c.push_back(v);
+			}
+		
+			void	pop(){
+
+				c.pop_back();
+			}
+
+			template<typename T1, typename C1>
+			friend bool	operator==(const stack<T1, C1> &x, const stack<T1, C1> &y);
+		
+			template<typename T1, typename C1>
+			friend bool	operator!=(const stack<T1, C1> &x, const stack<T1, C1> &y);
+
+			template<typename T1, typename C1>
+			friend bool	operator<(const stack<T1, C1> &x, const stack<T1, C1> &y);
+		
+			template<typename T1, typename C1>
+			friend bool	operator>(const stack<T1, C1> &x, const stack<T1, C1> &y);
+		
+			template<typename T1, typename C1>
+			friend bool	operator<=(const stack<T1, C1> &x, const stack<T1, C1> &y);
+		
+			template<typename T1, typename C1>
+			friend bool	operator>=(const stack<T1, C1> &x, const stack<T1, C1> &y);
 	};
+
+	template<typename Tp, typename Conatiner>
+	bool	operator==(const stack<Tp, Conatiner> &x, const stack<Tp, Conatiner> &y){
+
+		return (x.c == y.c);
+	}
+
+	template<typename Tp, typename Conatiner>
+	bool	operator!=(const stack<Tp, Conatiner> &x, const stack<Tp, Conatiner> &y){
+
+		return (!(x.c == y.c));
+	}
+	
+	template<typename Tp, typename Conatiner>
+	bool	operator<(const stack<Tp, Conatiner> &x, const stack<Tp, Conatiner> &y){
+
+		return (x.c < y.c);
+	}
+	
+	template<typename Tp, typename Conatiner>
+	bool	operator>(const stack<Tp, Conatiner> &x, const stack<Tp, Conatiner> &y){
+
+		return (y < x);
+	}
+
+	template<typename Tp, typename Conatiner>
+	bool	operator<=(const stack<Tp, Conatiner> &x, const stack<Tp, Conatiner> &y){
+
+		return (!(y < x));
+	}
+	
+	template<typename Tp, typename Conatiner>
+	bool	operator>=(const stack<Tp, Conatiner> &x, const stack<Tp, Conatiner> &y){
+		
+		return (!(x < y));
+	}
 }
 
 #endif
